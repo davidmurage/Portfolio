8629f9b2cdb3df75bbd39d562e670c21dc6e35fd
davidmurage <davidmurage415@gmail.com>
2023-12-12 12:07:42 +0300

    updated contact folder

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -142,7 +142,7 @@ const Contact = () => {
 
 
   return (
-    <Container>
+    <Container id="contact">
       <Wrapper>
         <Title>Contact</Title>
         <Desc>Feel free to reach out to me for any questions or opportunities!</Desc>
